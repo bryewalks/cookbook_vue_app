79c0bb4431443bf9afa54210f53c53df82192f0a
bryewalks <bryewalks@gmail.com>
2019-03-01 09:43:45 -0600

    development address

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios";
+import Vue2Filters from 'vue2-filters'
+import StarRating from 'vue-star-rating'
 
 axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://lit-shore-66444.herokuapp.com";
+
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  StarRating,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/RecipesIndex.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="recipes-index">
+    <h1>All Recipes</h1>
+    <div>
+      Filter Title: <input v-model="titleFilter" list="titles">
+    </div>
+
+    <datalist id="titles">
+      <option v-for="recipe in recipes">{{ recipe.title }}</option>
+    </datalist>
+
+    <div class="row">
+      <div v-for="recipe in filterBy(recipes, titleFilter, 'title')" class="col-md-4">
+        <h2>{{ recipe.title }}</h2>
+        <router-link v-bind:to="'/recipes/' + recipe.id">
+          <img v-bind:src="recipe.image_url" v-bind:alt="recipe.title">
+        </router-link>
+      </div>     
+    </div>
+    
+  </div>
+</template>
+
+<style>
+img{
+  width: 250px; 
+}
+</style>
+
+<script>
+var axios = require('axios');
+import Vue2Filters from'vue2-filters';
+
+export default {
+  data: function() {
+    return {
+      recipes: [],
+      currentRecipe: {},
+      titleFilter: "",
+    };
+  },
+  created: function() {
+    axios.get("/api/recipes")
+      .then(response => {
+        this.recipes = response.data;
+      });
+  },
+  methods: {
+  },
+  mixins: [Vue2Filters.mixin]
+};
+</script>
